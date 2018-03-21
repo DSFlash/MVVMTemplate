43bb0e2f7ec562be0f86729df49d562c4c07ca4f
DS <Dheeraj>
2018-03-21 17:28:37 +0530

    MVVM template commit

---
+++ MVVM.xctemplate/ownsView/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import UIKit
+
+class ___VARIABLE_productName___ViewController: UIViewController {
+
+    lazy var viewModel: ___VARIABLE_productName___ViewModel = {
+        return ___VARIABLE_productName___ViewModel()
+    }()
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
